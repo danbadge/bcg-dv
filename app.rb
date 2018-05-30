6950c0b9f601d24defb5a068387eaab0af5109b6
Daniel Watts <danbadge@gmail.com>
2018-05-30 19:49:52 +0100

    Add lots of specs and go with the simplest possible solution

--- app.rb
+++ app.rb
@@ -14,14 +14,37 @@ get '/lines' do
   param :x, Integer, :required => true
   param :y, Integer, :required => true
 
-  lines = CSV.read('data/lines.csv', :headers => true)
+  nearby_stop_ids = stop_ids_at_coodinates(params[:x], params[:y])
 
-  body = lines.map { |l|
+  stop_times = CSV.read('data/stop_times.csv', :headers => true)
+  stops_at_time_and_coordinates = stop_times.select { |times|
+    nearby_stop_ids.include?(times['stop_id']) && times['time'] == params[:timestamp]
+  }
+
+  line_ids = stops_at_time_and_coordinates.map { |times| times['line_id'] }
+
+  all_lines = CSV.read('data/lines.csv', :headers => true)
+
+  lines = all_lines.select { |line| line_ids.include?(line['line_id']) }
+
+  body = lines.map { |line|
     {
-      'id' => l['line_id'],
-      'name' => l['line_name'],
+      :id => line['line_id'],
+      :name => line['line_name'],
     }
   }
 
   body.to_json
+end
+
+def stop_ids_at_coodinates(x, y)
+  stops = CSV.read('data/stops.csv', :headers => true)
+
+  nearby_stops = stops.select { |stop|
+    stop['x'] == x.to_s && stop['y'] == y.to_s
+  }
+
+  nearby_stops.map { |stop|
+    stop['stop_id']
+  }
 end
--- spec/api/lines_spec.rb
+++ spec/api/lines_spec.rb
@@ -4,30 +4,109 @@ describe '~/lines' do
   let(:timestamp) { '10:00:00' }
   let(:x_coordinate) { '2' }
   let(:y_coordinate) { '4' }
+  let(:parsed_response_body) { JSON.parse(last_response.body) }
 
   before do
     get '/lines', :timestamp => timestamp, :x => x_coordinate, :y => y_coordinate
   end
 
-  context 'when making a request without timestamp' do
-    let(:timestamp) { nil }
-
-    it 'returns 400 bad request' do
-      expect(last_response.status).to eq(400)
+  describe 'given a valid request' do
+    it 'returns 200 OK' do
+      expect(last_response.status).to eq(200)
     end
 
-    it 'returns a helpful JSON error message' do
-      parsed_body = JSON.parse(last_response.body)
-      expected = {
-        'errors' => {'timestamp' => 'Parameter is required'},
-        'message' => 'Parameter is required',
-      }
-      expect(parsed_body).to eq(expected)
+    context 'when coordinates are 1,1 and timestamp is 10:00:00' do
+      let(:x_coordinate) { '1' }
+      let(:y_coordinate) { '1' }
+
+      it 'returns line number 0 called M4' do
+        expected = [{'id' => '0', 'name' => 'M4'}]
+        expect(parsed_response_body).to eq(expected)
+      end
+    end
+
+    context 'when coordinates match a valid stop that currently has not trains' do
+      let(:x_coordinate) { '1' }
+      let(:y_coordinate) { '10' }
+      let(:timestamp) { '10:09:00' }
+
+      it 'does not return a line' do
+        expect(parsed_response_body).to eq([])
+      end
+    end
+
+    context 'when coordinates are 191,828' do
+      let(:x_coordinate) { '191' }
+      let(:y_coordinate) { '828' }
+
+      it 'does not return a line' do
+        expect(parsed_response_body).to eq([])
+      end
+    end
+
+    context 'when timestamp is 24:00:00' do
+      let(:timestamp) { '24:00:00' }
+
+      it 'does not return a line' do
+        expect(parsed_response_body).to eq([])
+      end
+    end
+
+    context 'when coordinates are 2,9 and timestamp is 10:08:00' do
+      let(:x_coordinate) { '2' }
+      let(:y_coordinate) { '9' }
+      let(:timestamp) { '10:08:00' }
+
+      it 'returns the correct two lines' do
+        expected = [
+          {'id' => '1', 'name' => '200'},
+          {'id' => '2', 'name' => 'S75'},
+        ]
+        expect(parsed_response_body).to eq(expected)
+      end
+    end
+
+    context 'when coordinates are 2,9 and timestamp is 10:07:00' do
+      let(:x_coordinate) { '2' }
+      let(:y_coordinate) { '9' }
+      let(:timestamp) { '10:07:00' }
+
+      it 'returns line number 0 called M4' do
+        expected = [{'id' => '0', 'name' => 'M4'}]
+        expect(parsed_response_body).to eq(expected)
+      end
+    end
+
+    context 'when coordinates are 3,11 and timestamp is 10:11:00' do
+      let(:x_coordinate) { '3' }
+      let(:y_coordinate) { '11' }
+      let(:timestamp) { '10:11:00' }
+
+      it 'returns line number 0 called M4' do
+        expected = [{'id' => '2', 'name' => 'S75'}]
+        expect(parsed_response_body).to eq(expected)
+      end
     end
   end
 
-  describe 'given the x coordinate parameter is invalid' do
-    context 'when the x coordinate is missing' do
+  describe 'given an invalid request' do
+    context 'when the timestamp parameter is missing' do
+      let(:timestamp) { nil }
+
+      it 'returns 400 bad request' do
+        expect(last_response.status).to eq(400)
+      end
+
+      it 'returns a helpful JSON error message' do
+        expected = {
+          'errors' => {'timestamp' => 'Parameter is required'},
+          'message' => 'Parameter is required',
+        }
+        expect(parsed_response_body).to eq(expected)
+      end
+    end
+
+    context 'without the x coordinate parameter' do
       let(:x_coordinate) { nil }
 
       it 'returns 400 bad request' do
@@ -35,12 +114,11 @@ describe '~/lines' do
       end
 
       it 'returns a helpful JSON error message' do
-        parsed_body = JSON.parse(last_response.body)
-        expect(parsed_body['errors']).to eq('x' => 'Parameter is required')
+        expect(parsed_response_body['errors']).to eq('x' => 'Parameter is required')
       end
     end
 
-    context 'when the x coordinate is a string' do
+    context 'when the x coordinate parameter is a string' do
       let(:x_coordinate) { 'hello x' }
 
       it 'returns 400 bad request' do
@@ -48,14 +126,11 @@ describe '~/lines' do
       end
 
       it 'returns a helpful JSON error message' do
-        parsed_body = JSON.parse(last_response.body)
-        expect(parsed_body['errors']).to eq('x' => "'hello x' is not a valid Integer")
+        expect(parsed_response_body['errors']).to eq('x' => "'hello x' is not a valid Integer")
       end
     end
-  end
 
-  describe 'given the y coordinate parameter is invalid' do
-    context 'when the y coordinate is missing' do
+    context 'when the y coordinate parameter is missing' do
       let(:y_coordinate) { nil }
 
       it 'returns 400 bad request' do
@@ -63,12 +138,11 @@ describe '~/lines' do
       end
 
       it 'returns a helpful JSON error message' do
-        parsed_body = JSON.parse(last_response.body)
-        expect(parsed_body['errors']).to eq('y' => 'Parameter is required')
+        expect(parsed_response_body['errors']).to eq('y' => 'Parameter is required')
       end
     end
 
-    context 'when the y coordinate is a string' do
+    context 'when the y coordinate parameter is a string' do
       let(:y_coordinate) { 'hello y' }
 
       it 'returns 400 bad request' do
@@ -76,25 +150,8 @@ describe '~/lines' do
       end
 
       it 'returns a helpful JSON error message' do
-        parsed_body = JSON.parse(last_response.body)
-        expect(parsed_body['errors']).to eq('y' => "'hello y' is not a valid Integer")
+        expect(parsed_response_body['errors']).to eq('y' => "'hello y' is not a valid Integer")
       end
-    end
-  end
-
-  context 'when making a valid request' do
-    it 'returns 200 OK' do
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'returns all possible lines' do
-      parsed_body = JSON.parse(last_response.body)
-      expected = [
-        {'id' => '0', 'name' => 'M4'},
-        {'id' => '1', 'name' => '200'},
-        {'id' => '2', 'name' => 'S75'},
-      ]
-      expect(parsed_body).to eq(expected)
     end
   end
 end
